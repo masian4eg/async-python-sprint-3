fe3629676825694e31c701cb9bd1c3d3984f473f
masian4eg <masian28@gmail.com>
2023-05-12 00:11:27 +0700

    rev

--- client.py
+++ client.py
@@ -1,10 +1,49 @@
-# import asyncio
-# import aiohttp
+import asyncio
+import logging
+
+import websockets
+from websockets import WebSocketClientProtocol
+import aiohttp
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Client:
     def __init__(self, server_host="127.0.0.1", server_port=8000):
-        pass
+        self.server_host = server_host
+        self.server_port = server_port
 
-    def send(self, message=""):
-        pass
+    # async def handler(self, websocket: WebSocketClientProtocol) -> None:
+    #     async for message in websocket:
+    #         logger.info(f"Message: {message}")
+
+    async def send(self):
+        # msg = input("msg: ")
+        # async with websockets.connect(f"ws://{self.server_host}:{self.server_port}") as socket:
+        #     await socket.send(msg)
+        #     await socket.recv()
+        session = aiohttp.ClientSession()
+        async with session.ws_connect(f"ws://{self.server_host}:{self.server_port}") as ws:
+            async for msg in ws:
+                if msg.type == aiohttp.WSMsgType.TEXT:
+                    if msg.data == 'close cmd':
+                        await ws.close()
+                        break
+                    else:
+                        await ws.send_str(msg.data + '/answer')
+                elif msg.type == aiohttp.WSMsgType.ERROR:
+                    break
+
+    # def run(self):
+    #     event_loop = asyncio.get_event_loop()
+    #     event_loop.run_until_complete(self.send())
+    #     event_loop.run_forever()
+
+
+if __name__ == "__main__":
+    client = Client()
+    # client.run()
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(client.send())
+    event_loop.run_forever()
--- server.py
+++ server.py
@@ -1,9 +1,56 @@
-# import asyncio
+import asyncio
+import logging
+
+import websockets
+from websockets import WebSocketServerProtocol
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Server:
     def __init__(self, host="127.0.0.1", port=8000):
-        pass
+        self.host = host
+        self.port = port
+        self.clients = set()
 
-    def listen(self):
-        pass
+    async def register(self, ws: WebSocketServerProtocol) -> None:
+        self.clients.add(ws)
+        logger.info(f"{ws.remote_address} connects.")
+
+    async def unregister(self, ws: WebSocketServerProtocol) -> None:
+        self.clients.remove(ws)
+        logger.info(f"{ws.remote_address} disconnects.")
+
+    async def send_message(self, message: str = ""):
+        if self.clients:
+            await asyncio.wait([client.send(message) for client in self.clients])
+
+    async def ws_handler(self, ws: WebSocketServerProtocol, url: str) -> None:
+        await self.register(ws)
+        await self.distribute(ws)
+        # try:
+        #     await self.distribute(ws)
+        # finally:
+        #     await self.unregister(ws)
+
+    async def distribute(self, ws: WebSocketServerProtocol) -> None:
+        async for message in ws:
+            await self.send_message(message)
+
+    # async def serve(self):
+    #     await websockets.serve(self.ws_handler, self.host, self.port)
+    #
+    # def listen(self):
+    #     event_loop = asyncio.get_event_loop()
+    #     event_loop.run_until_complete(self.serve())
+    #     event_loop.run_forever()
+
+
+if __name__ == "__main__":
+    server = Server()
+    # server.listen()
+    start_server = websockets.serve(server.ws_handler, "127.0.0.1", 8000)
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(start_server)
+    event_loop.run_forever()
